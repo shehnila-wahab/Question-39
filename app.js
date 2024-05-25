e90a65f148cab570a9ebe7b876cf0d594761346b
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:48:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function city_country(city, country) {
+    // return`${city},${country}`
+    console.log("\"".concat(city, "\",").concat(country));
+}
+city_country("karachi", "pakistan");
+var mycities = city_country("karachi", "pakistan");
+console.log(mycities);
+console.log(city_country("Dubai", "USA"));
+console.log(city_country("london", "united kingdom"));
+console.log(city_country("Riyaz", "saudia Arabia"));
---
+++ app.ts
@@ -0,0 +1,11 @@
+function city_country(city:string, country: string){
+    // return`${city},${country}`
+console.log(`"${city}",${country}`)
+}
+city_country("karachi","pakistan");
+
+let mycities= city_country("karachi","pakistan");
+console.log(mycities);
+console.log(city_country("Dubai","USA"));
+console.log(city_country("london","united kingdom"));
+console.log(city_country("Riyaz","saudia Arabia"));
